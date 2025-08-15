d65debbe119238110d200148c0572307a1a93128
INYANG UTIBE BASSEY <bassey.utibe22@gmail.com>
2025-08-15 04:16:13 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -14,12 +14,20 @@ print(z)
 '''
 The example so far has relied on simple naming conventions for variables 
 such as X, Y, and Z. However, as programs grow in complexity, it's important 
-to adopt clear and consistent naming practices. When working on a project with
-other developers, using descriptive variable names can greatly enhance code readability
-and maintainability. Using generic variable names like X, Y, and Z can make code harder
+to adopt clear and consistent naming practices. When working on a project 
+with
+other developers, using descriptive variable names can greatly enhance code 
+readability
+and maintainability. Using generic variable names like X, Y, and Z can make 
+code harder
 to understand and maintain over time.
 
-As a programmer, it is important to know that data will change throughout the lifecycle of a program. This means that variable names should be chosen with care, as they may need to be updated or modified as the program evolves. By using descriptive names and following consistent naming conventions, you can make your code more adaptable and easier to work with in the long run.
+As a programmer, it is important to know that data will change throughout 
+the lifecycle of a program. This means that variable names should be chosen 
+with care, as they may need to be updated or modified as the program 
+evolves. By using descriptive names and following consistent naming 
+conventions, you can make your code more adaptable and easier to work with 
+in the long run.
 '''
 
 first_name = "Utibe"
@@ -30,7 +38,7 @@ print(first_name)
 print(last_name)
 print(age)
 
-
+#Another example.
 a,b,c = 1, 2, 3
 print(a)
 print(b)
