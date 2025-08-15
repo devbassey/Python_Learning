ac4cc5fadee7d5488ed6cc3d07e0b5996ddb5815
INYANG UTIBE BASSEY <bassey.utibe22@gmail.com>
2025-08-15 04:08:08 +0100

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,12 +1,31 @@
-# VARIABLES are used to store all kinds of data. They are the cornerstone of programming.
+'''
+MyNOTE!
+VARIABLES are used to store all kinds of data. They are the 
+cornerstone of programming. Declaring variables in python is straightforward.
+All you need to do is declare a name and assign it a value as seen below.
+
+The word VARIABLE refers to something that can change or vary.
+'''
 x = 3000
 y = 1000
 z = x + y
 print(z)
 
-# FUNCTIONS
-def add_numbers(a, b):
-    return a + b
+'''
+The example so far has relied on simple naming conventions for variables 
+such as X, Y, and Z. However, as programs grow in complexity, it's important 
+to adopt clear and consistent naming practices. When working on a project with
+other developers, using descriptive variable names can greatly enhance code readability
+and maintainability. Using generic variable names like X, Y, and Z can make code harder
+to understand and maintain over time.
 
-result = add_numbers(x, y)
-print(result)
+As a programmer, it is important to know that data will change throughout the lifecycle of a program. This means that variable names should be chosen with care, as they may need to be updated or modified as the program evolves. By using descriptive names and following consistent naming conventions, you can make your code more adaptable and easier to work with in the long run.
+'''
+
+first_name = "Utibe"
+last_name = "Bassey"
+age = 30
+
+print(first_name)
+print(last_name)
+print(age)
