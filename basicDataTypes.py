efd34ca75e22bffcc3779d5c4639b1a3e5dd5d1c
INYANG UTIBE BASSEY <bassey.utibe22@gmail.com>
2025-08-15 04:31:45 +0100

    Update basicDataTypes.py

---
+++ basicDataTypes.py
@@ -0,0 +1,24 @@
+'''
+BASIC DATA TYPES
+Computer systems need to interpret data in a way that is meaningful and 
+useful. This involves understanding the different types of data that can be
+processed and how they can be represented in a computer's memory.
+
+A data type is a classification that specifies which type of value a 
+variable can hold. In programming, data types are important because they 
+determine the operations that can be performed on a particular piece of data. 
+Common data types include integers, floating-point numbers, strings, and 
+booleans.
+
+Python offers raw data types to allow data to be assigned to variables or
+constants. The five main types which are classed as literals includes;
+1. Numeric (Integer, Float, Complex Numbers)
+2. Sequence (String, List, Tuple)
+3. Dictionary (Key-Value Pairs)
+4. Set (Unique Unordered Collection)
+5. Boolean (True or False)
+
+In programming, you need to decide what type would best suit your data and 
+the operations you want to perform on it. Choosing the right data type is 
+crucial for optimizing performance and ensuring the correctness of your code.
+'''
