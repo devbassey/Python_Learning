3d47157c044c5994d7fc485411ba5b374d8e1af8
INYANG UTIBE BASSEY <bassey.utibe22@gmail.com>
2025-08-15 03:47:49 +0100

    Update variables.py

---
+++ variables.py
@@ -0,0 +1,12 @@
+# VARIABLES are used to store all kinds of data. They are the cornerstone of programming.
+x = 3000
+y = 1000
+z = x + y
+print(z)
+
+# FUNCTIONS
+def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(x, y)
+print(result)
